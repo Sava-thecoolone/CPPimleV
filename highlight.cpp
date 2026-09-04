ed39a2d169afaf2976b2acfa687b8609455440bf
Sava-thecoolone <saveliyakov0401@gmail.com>
2026-09-05 00:14:28 +0400

    use std::atomic

--- highlight.cpp
+++ highlight.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <atomic>
 
 struct highlight {
     int len;
-    int cur;
+    std::atomic<int> cur;
     double delayMult;
     int unslept;
 
     highlight() {
         this->len = 1;
-        cur = -1;
+        cur.store(-1);
         delayMult = 1;
         unslept = 0;
     }
 
     highlight(int len) {
         this->len = len;
-        cur = -1;
+        cur.store(-1);
         delayMult = 1;
         unslept = 0;
     }
 
     void doHigh(int idx) {
-        cur = idx;
+        cur.store(idx);
         int delay = (int)((100000000.0*delayMult)/len);
         if (delay+unslept > 1000000) {
             std::this_thread::sleep_for(std::chrono::milliseconds((delay+unslept)/1000000));
