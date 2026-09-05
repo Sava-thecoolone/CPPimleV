61eb548683b9b7c2a2c30ebb1ce52c96a53c228f
Sava <saveliyakov0401@gmail.com>
2026-09-05 17:38:52 +0400

    better sounds

--- sounds.cpp
+++ sounds.cpp
@@ -14,15 +14,15 @@ void doaudio(void *buffer, unsigned int frames) {
 void setupaudiothread(varray &arr) {
     std::thread audio([&] {
         while (!WindowShouldClose()) {
+            tsf_channel_note_off_all(soundfont, 0);
             int cur = arr.high.cur.load();
             if (cur != -1) {
-                double note = ((double)arr.values[cur]/arr.len)*80+20;
-                tsf_channel_note_on(soundfont, 0, note, 1.0f);
+                double note = ((double)arr.values[cur]/arr.len)*80+25;
+                tsf_channel_note_on(soundfont, 0, note, 1.0f); // from ArrayV
                 tsf_channel_set_pitchwheel(soundfont, 0, (int)((note-((int)note))*8192.0)+8192);
                 tsf_channel_midi_control(soundfont, 0, 91, 10);
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            tsf_channel_midi_control(soundfont, 0, 123, 0);
         }
     });
     audio.detach();
@@ -34,13 +34,13 @@ AudioStream setupsound(varray &arr) {
     if (!soundfont) {
         throw std::runtime_error("no sfx.sf2");
     }
-    tsf_set_output(soundfont, TSF_MONO, 44100, 0.0f);
-    tsf_channel_set_presetnumber(soundfont, 0, 16, 0);
+    tsf_set_output(soundfont, TSF_STEREO_INTERLEAVED, 44100, 0.0f);
+    tsf_channel_set_presetnumber(soundfont, 0, 17, 0);
     tsf_channel_midi_control(soundfont, 0, 72, 127);
     // for (int i = 0; i < tsf_get_presetcount(soundfont); i++) {
     //     std::cout << i << " : " << tsf_bank_get_presetname(soundfont, 0, i) << "\n";
     // }
-    AudioStream stream = LoadAudioStream(44100, 32, 1);
+    AudioStream stream = LoadAudioStream(44100, 32, 2);
     SetAudioStreamCallback(stream, doaudio);
     PlayAudioStream(stream);
     return stream;
